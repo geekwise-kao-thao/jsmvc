9f2e6ade7cf3defc1c4358249af06bad4f83a0e6
Kao Thao <kausthoj@gmail.com>
2016-05-24 23:58:41 +0000

    small crud to kick off mvc exercise

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var get_element = function (element_id){
+    return document.getElementById(element_id);
+};
+
+// model
+
+var name_array = [];
+
+// view
+var create_view_elements = function(element_type,element_id,text_content){
+    var element = document.createElement(element_type);
+    element.setAttribute('id',element_id);
+    element.textContent = text_content;
+    document.body.appendChild(element);
+};
+
+// controller
+
+var display_names = function (){
+    get_element('input_box').addEventListener('keydown',function(event){
+        if(event.keyCode === 13){
+            name_array.push(this.value);
+            console.log(name_array);
+                create_view_elements('p','',name_array[name_array.length-1]);
+                this.value = null;
+        };
+    });
+};
+
+var display_none_names = function(event){
+    event.currentTarget.style.display = "none";
+};
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    create_view_elements('h1','caption_1','Type in name then press ENTER');
+    create_view_elements('input','input_box','');
+    create_view_elements('h1','caption_2','CLICK on the name to delete');
+
+    get_element('input_box').setAttribute('placeholder','First Name');
+
+    display_names();
+
+});
